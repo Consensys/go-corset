98d24299869343c2859e41f2c3d27172df87971b
DavePearce <dave01001110@gmail.com>
2024-03-27 17:54:18 +1300

    Very simplistic JSON deserisalizer

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,34 @@
 package main
+
+
+import (
+	"fmt"
+	"encoding/json"
+	"os"
+)
+
+// type ConstraintSet struct {
+// 	columns string
+// 	constraints string
+// 	constants string
+// 	computations string
+// 	perspectives []string
+// 	transformations uint64
+// 	auto_constraints uint64
+// }
+
+func main() {
+	file := os.Args[1]
+	//
+	fmt.Printf("Reading JSON bin file: %s\n",file)
+	bytes, err := os.ReadFile(file)
+	if err != nil {
+		fmt.Println("Error")
+	} else {
+		//var constraints ConstraintSet
+		var constraints map[string]interface{}
+		err := json.Unmarshal(bytes, &constraints)
+		if err != nil { fmt.Println(err) }
+		fmt.Printf("%s",constraints)
+	}
+}
