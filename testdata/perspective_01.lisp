33b1dd2ca83814fe764f20e2202caef23bfe1623
DavePearce <dave01001110@gmail.com>
2024-12-22 22:30:03 +1300

    Add initial perspective test
    This adds an initial perspective test, along with trace inputs.

---
+++ testdata/perspective_01.lisp
@@ -0,0 +1,15 @@
+(defcolumns
+    ;; Column (not in perspective)
+    A
+    ;; Selector column for perspective p1
+    (P :binary@prove)
+    ;; Selector column for perspective p2
+    (Q :binary@prove))
+
+;; Section 1
+(defperspective p1 P ((B :byte)))
+(defconstraint c1 (:perspective p1) (vanishes! (- A B)))
+
+;; Section 2
+(defperspective p2 Q ((C :byte)))
+(defconstraint c2 (:perspective p2) (vanishes! (* A C)))
