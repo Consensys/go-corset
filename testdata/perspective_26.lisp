8b9ab5eef6086a5152cbf173afff1eba99d7e363
David Pearce <david.pearce@consensys.net>
2025-01-09 11:10:36 +1300

    fix: parsing of array accesses (#509)
    Fix parsing of array accesses
    
    A simple fix for #505, which was being caused by the parser not
    considering qualified names correctly.

---
+++ testdata/perspective_26.lisp
@@ -0,0 +1,19 @@
+(defpurefun ((vanishes! :@loob :force) e0) e0)
+;;
+(defcolumns
+    ;; Column (not in perspective)
+    A
+    ;; Selector column for perspective p1
+    (P :binary@prove)
+    ;; Selector column for perspective p2
+    (Q :binary@prove))
+
+;; Section 1
+(defperspective p1 P ((B :binary)))
+(defun (p1-B) p1/B)
+(defconstraint c1 (:perspective p1) (vanishes! (- A (p1-B))))
+
+;; Section 2
+(defperspective p2 Q ((C :binary)))
+(defun (p2-C) p2/C)
+(defconstraint c2 (:perspective p2) (vanishes! (* A (p2-C))))
---
+++ testdata/perspective_27.lisp
@@ -0,0 +1,15 @@
+(defpurefun ((vanishes! :@loob :force) e0) e0)
+;;
+(defcolumns
+    ;; Selector column for perspective p1
+    (P :binary@prove)
+    ;; Selector column for perspective p2
+    (Q :binary@prove))
+
+;; Section 1
+(defperspective p1 P ((B :binary :array [2])))
+(defconstraint c1 (:perspective p1) (vanishes! (- [p1/B 1] [p1/B 2])))
+
+;; Section 2
+(defperspective p2 Q ((C :binary :array [2])))
+(defconstraint c2 (:perspective p2) (vanishes! (* [p2/C 1] [p2/C 2])))
