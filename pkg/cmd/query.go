ce8a0f7c952d929b45f3903db87be42eefaba1d4
amkCha <29160563+amkCha@users.noreply.github.com>
2025-01-27 17:32:18 +0100

    feat(query): add flag for field type

--- pkg/cmd/query.go
+++ pkg/cmd/query.go
@@ -10,27 +10,29 @@ var queryCmd = &cobra.Command{
 	Short: "Query information from a binary package.",
 	Long:  `Query specific information from the binary package.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// field := GetFlag(cmd, "field-columns")
+		field := GetFlag(cmd, "field-columns")
+		fieldType := GetString(cmd, "field-type")
 		// Parse constraints
 		hirSchema := readSchema(true, false, true, args[0:])
-		// if field {
-		schemaCols := hirSchema.Columns()
-		// Check each column
-		for i := uint(0); i < schemaCols.Count(); i++ {
-			scCol := schemaCols.Next()
-			// Extract type for ith column
-			colType := scCol.DataType
-			// If type field
-			if colType.String() == "u128" {
-				fmt.Println(scCol.Name)
+		if field {
+			schemaCols := hirSchema.Columns()
+			// Check each column
+			for i := uint(0); i < schemaCols.Count(); i++ {
+				scCol := schemaCols.Next()
+				// Extract type for ith column
+				colType := scCol.DataType
+				// If type field
+				if colType.String() == fieldType {
+					fmt.Println(scCol.Name)
+				}
 			}
 		}
-		// }
 	},
 }
 
 //nolint:errcheck
 func init() {
 	rootCmd.AddCommand(queryCmd)
-	// checkCmd.Flags().Bool("field-columns", false, "list columns name of field type")
+	queryCmd.Flags().Bool("field-columns", false, "list column names of field type. ")
+	queryCmd.Flags().String("field-type", "u128", "specify field type. Default is field type u128 for BLS12-377.")
 }
