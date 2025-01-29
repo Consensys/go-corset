f6738fc6f37d279805d44eb942bb38a517edbd11
DavePearce <dave01001110@gmail.com>
2025-01-29 14:03:35 +1300

    fix for defcompute typing
    This puts in place a straightforward fix for typing defcompute
    constructs.  The problem was that, at the corset level, the type of the
    target column was being extracted from the wrong input column.
    Amazingly, the tests all still passed because some of the input columns
    and unrestrictive (field) types.  Have added tests which do fail with
    this error.

--- testdata/native_10.lisp
+++ testdata/native_10.lisp
@@ -1,5 +1,5 @@
 (defpurefun ((eq! :@loob) x y) (- x y))
 
-(defcolumns P Q (X :i8) Y)
+(defcolumns (P :binary) (Q :binary) (X :i8) Y)
 (defcomputed (Z) (fwd-fill-within P Q X))
 (defconstraint c1 () (eq! Y Z))
--- testdata/native_11.lisp
+++ testdata/native_11.lisp
@@ -1,5 +1,5 @@
 (defpurefun ((eq! :@loob) x y) (- x y))
 
-(defcolumns P Q (X :i16) Y)
+(defcolumns (P :binary) (Q :binary) (X :i16) Y)
 (defcomputed (Z) (bwd-fill-within P Q X))
 (defconstraint c1 () (eq! Y Z))
