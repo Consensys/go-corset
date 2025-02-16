ddce56250d29460809592ec08e931ae61b39e687
David Pearce <david.pearce@consensys.net>
2025-02-17 12:56:18 +1300

    chore: add tests for inverse optimisations (#701)
    add tests for inverse optimisations
    
    This adds some tests which demonstrate the effectiveness of various
    inverse optimisations being considered.

---
+++ testdata/if_16.lisp
@@ -0,0 +1,8 @@
+(defpurefun ((vanishes! :@loob) x) x)
+(defcolumns (A :i2@loob@prove) B C)
+
+;; returns non-zero value if A is zero
+(defun (isz-A) (* (- A 1) (- A 2) (- A 3)))
+
+(defconstraint c1 () (vanishes! (* (isz-A) B)))
+(defconstraint c2 () (vanishes! (* A C)))
