df077c7d4de43593b25b211a8a4ddc4f2377c36c
DavePearce <dave01001110@gmail.com>
2025-03-10 15:40:12 +1300

    update permute tests

--- testdata/permute_03.lisp
+++ testdata/permute_03.lisp
@@ -1,5 +1,5 @@
 (defpurefun ((vanishes! :𝔽@loob) x) x)
-(defcolumns ST (X :i16@prove))
+(defcolumns (ST :i4) (X :i16@prove))
 (defpermutation (Y) ((↓ X)))
 ;; Ensure sorted column increments by 1
 (defconstraint increment ()
