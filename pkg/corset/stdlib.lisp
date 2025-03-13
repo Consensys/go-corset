8a320a33e247d6f81a6154363179684490f46dea
DavePearce <dave01001110@gmail.com>
2025-03-14 11:17:48 +1300

    trim down the stdlib

--- pkg/corset/stdlib.lisp
+++ pkg/corset/stdlib.lisp
@@ -30,8 +30,8 @@
 (defpurefun ((or! :𝔽@loob) a b) (* a b))
 (defpurefun ((not :binary@bool :force) (x :binary)) (- 1 x))
 
-(defpurefun ((eq! :𝔽@loob) x y) (- x y))
-(defpurefun ((neq! :binary@loob :force) x y) (not (~ (eq! x y))))
+(defpurefun ((eq! :𝔽@loob) x y) (== x y))
+(defpurefun ((neq! :binary@loob :force) x y) (!= x y))
 (defunalias = eq!)
 
 (defpurefun ((eq :binary@bool :force) x y) (- 1 (~ (eq! x y))))
@@ -57,34 +57,19 @@
 (defpurefun (will-inc! e0 offset) (will-eq! e0 (+ e0 offset)))
 (defpurefun (will-dec! e0 offset) (eq! (next e0) (- e0 offset)))
 
-(defpurefun (did-inc e0 offset) (eq e0 (+ (prev e0) offset)))
-(defpurefun (did-dec e0 offset) (eq  e0 (- (prev e0) offset)))
-(defpurefun (will-inc e0 offset) (will-eq e0 (+ e0 offset)))
-(defpurefun (will-dec e0 offset) (eq (next e0) (- e0 offset)))
-
 ;; Ensure that e0 remained (resp. will be) constant
 ;; with regards to the previous (resp. next) row.
 (defpurefun (remained-constant! e0) (eq! e0 (prev e0)))
 (defpurefun (will-remain-constant! e0) (will-eq! e0 e0))
 
-(defpurefun (remained-constant e0) (eq e0 (prev e0)))
-(defpurefun (will-remain-constant e0) (will-eq e0 e0))
-
 ;; Ensure (in loobean logic) that e0 has changed (resp. will change) its value
 ;; with regards to the previous (resp. next) row.
 (defpurefun (did-change! e0) (neq! e0 (prev e0)))
-(defpurefun (will-change! e0) (neq! e0 (next e0)))
-
-(defpurefun (did-change e0) (neq e0 (prev e0)))
-(defpurefun (will-change e0) (neq e0 (next e0)))
 
 ;; Ensure (in loobean logic) that e0 was (resp. will be) equal to e1 in the
 ;; previous (resp. next) row.
 (defpurefun (was-eq! e0 e1) (eq! (prev e0) e1))
 (defpurefun (will-eq! e0 e1) (eq! (next e0) e1))
-
-(defpurefun (was-eq e0 e1) (eq (prev e0) e1))
-(defpurefun (will-eq e0 e1) (eq (next e0) e1))
 
 ;; Helpers
 (defpurefun ((vanishes! :𝔽@loob :force) e0) e0)
