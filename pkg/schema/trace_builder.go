ba33f85449f61c5b95c62857cf31d3eee7d6380e
DavePearce <dave01001110@gmail.com>
2025-06-16 11:24:25 +1200

    reduce memory usage for trace validation
    Trace validation was consuming an unreasonable amount of memory
    converting fr.Element instances into big.Int instances for the
    comparison.  This was incredibly wasteful!  Fixed now.

--- pkg/schema/trace_builder.go
+++ pkg/schema/trace_builder.go
@@ -15,7 +15,6 @@ package schema
 import (
 	"fmt"
 	"math"
-	"math/big"
 
 	"github.com/consensys/gnark-crypto/ecc/bls12-377/fr"
 	"github.com/consensys/go-corset/pkg/trace"
@@ -676,19 +675,14 @@ func fillComputedColumns(refs []RegisterRef, cols []tr.ArrayColumn, trace *tr.Ar
 
 // Validate that all elements of a given column fit within a given bitwidth.
 func validateColumnBitWidth(bitwidth uint, col trace.Column, mod Module) error {
-	var biBound big.Int
+	var frBound fr.Element = fr.NewElement(2)
 	// Compute 2^n
-	biBound.Exp(big.NewInt(2), big.NewInt(int64(bitwidth)), nil)
+	util.Pow(&frBound, uint64(bitwidth))
 	//
 	for j := 0; j < int(col.Data().Len()); j++ {
-		var (
-			bi  big.Int
-			jth = col.Get(j)
-		)
-		// Convert field element to bigint
-		jth.BigInt(&bi)
+		var jth = col.Get(j)
 		//
-		if bi.Cmp(&biBound) >= 0 {
+		if jth.Cmp(&frBound) >= 0 {
 			qualColName := trace.QualifiedColumnName(mod.Name(), col.Name())
 			return fmt.Errorf("row %d of column %s is out-of-bounds (%s)", j, qualColName, jth.String())
 		}
