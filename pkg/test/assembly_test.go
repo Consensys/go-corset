fbd9a0177851f55b71ecc296d9a6c9c61eecac9a
David Pearce <david.pearce@consensys.net>
2025-08-08 10:08:53 +1200

    feat: first version of log256 function (#1091)
    first version of log256 function
    
    This version uses a very simple linear loop.  However, we should be
    able to do better!

--- pkg/test/assembly_test.go
+++ pkg/test/assembly_test.go
@@ -54,6 +54,11 @@ func Test_Asm_FastPow(t *testing.T) {
 //	func Test_Asm_Inc(t *testing.T) {
 //		test_util.CheckWithFields(t, false, "asm/inc", sc.BLS12_377, sc.GF_8209, sc.GF_251)
 //	}
+
+func Test_Asm_Log256(t *testing.T) {
+	test_util.Check(t, false, "asm/log256")
+}
+
 func Test_Asm_Max14(t *testing.T) {
 	test_util.CheckWithFields(t, false, "asm/max14", sc.BLS12_377, sc.GF_8209, sc.GF_251)
 }
