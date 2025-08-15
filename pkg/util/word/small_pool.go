4df8eec0e2801fd68ee60792a91a5da68d4d5fdc
DavePearce <dave01001110@gmail.com>
2025-08-15 21:20:22 +1200

    ensure small pools are really static
    The small were causing significant overhead for the tests because they
    were begin freshly allocated for each test.

--- pkg/util/word/small_pool.go
+++ pkg/util/word/small_pool.go
@@ -12,6 +12,11 @@
 // SPDX-License-Identifier: Apache-2.0
 package word
 
+import (
+	"github.com/consensys/go-corset/pkg/util/field/bls12_377"
+	"github.com/consensys/go-corset/pkg/util/field/koalabear"
+)
+
 // SmallPool is a pool implementation for handling small domains (i.e. where all
 // elements can be enumerated).
 type SmallPool[K uint8 | uint16, W Word[W]] struct {
@@ -21,12 +26,7 @@ type SmallPool[K uint8 | uint16, W Word[W]] struct {
 // NewBytePool constructs a new pool which uses a given key type for
 // indexing.
 func NewBytePool[W Word[W]]() SmallPool[uint8, W] {
-	index := make([]W, 256)
-	//
-	for i := range uint64(256) {
-		var ith W
-		index[i] = ith.SetUint64(i)
-	}
+	index := getIndex[W]()
 	//
 	return SmallPool[uint8, W]{index}
 }
@@ -34,12 +34,7 @@ func NewBytePool[W Word[W]]() SmallPool[uint8, W] {
 // NewWordPool constructs a new pool which uses a given key type for
 // indexing.
 func NewWordPool[W Word[W]]() SmallPool[uint16, W] {
-	index := make([]W, 65536)
-	//
-	for i := range uint64(65536) {
-		var ith W
-		index[i] = ith.SetUint64(i)
-	}
+	index := getIndex[W]()
 	//
 	return SmallPool[uint16, W]{index}
 }
@@ -57,4 +52,54 @@ func (p SmallPool[K, F]) Put(element F) K {
 // Clone implementation for Pool interface.
 func (p SmallPool[K, F]) Clone() Pool[K, F] {
 	return p
+}
+
+// ============================================================================
+// Custom implementations
+// ============================================================================
+
+// Index for the bls12_377 curve
+var bls12_377_index []bls12_377.Element
+
+// Index for the koalabear curve
+var koalabear_index []koalabear.Element
+
+// Index for the bigendian word
+var bigendian_index []BigEndian
+
+func getIndex[W Word[W]]() []W {
+	var (
+		dummy W
+		index []W
+	)
+	//
+	switch any(dummy).(type) {
+	case BigEndian:
+		index = any(bigendian_index).([]W)
+	case bls12_377.Element:
+		index = any(bls12_377_index).([]W)
+	case koalabear.Element:
+		index = any(koalabear_index).([]W)
+	default:
+		panic("small pool not supported")
+	}
+	//
+	return index
+}
+
+func initIndex[W Word[W]]() []W {
+	var index = make([]W, 65536)
+	//
+	for i := range uint64(65536) {
+		var ith W
+		index[i] = ith.SetUint64(i)
+	}
+	//
+	return index
+}
+
+func init() {
+	bigendian_index = initIndex[BigEndian]()
+	bls12_377_index = initIndex[bls12_377.Element]()
+	koalabear_index = initIndex[koalabear.Element]()
 }
