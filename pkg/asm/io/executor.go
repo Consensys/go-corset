4b8b3986b524c58ea0780ae6597ebed047103c93
DavePearce <dave01001110@gmail.com>
2025-09-15 10:22:52 +1200

    Split out FunctionTrace from Executor
    This splits out a separate object for capturing a function's trace
    within the executor.  The benefit of this is that we can now lock data
    within an individual trace.

--- pkg/asm/io/executor.go
+++ pkg/asm/io/executor.go
@@ -20,6 +20,130 @@ import (
 	"github.com/consensys/go-corset/pkg/util/collection/set"
 )
 
+// Executor provides a mechanism for executing a program efficiently and
+// generating a suitable top-level trace.  Executor implements the io.Map
+// interface.
+type Executor[T Instruction[T]] struct {
+	functions []*FunctionTrace[T]
+}
+
+// NewExecutor constructs a new executor.
+func NewExecutor[T Instruction[T]](program Program[T]) *Executor[T] {
+	// Initialise executor traces
+	traces := make([]*FunctionTrace[T], len(program.Functions()))
+	//
+	for i := range traces {
+		traces[i] = NewFunctionTrace(program.functions[i])
+	}
+	// Construct new executor
+	return &Executor[T]{traces}
+}
+
+// Instance returns a valid instance of the given bus.
+func (p *Executor[T]) Instance(bus uint) FunctionInstance {
+	var (
+		fn     = p.functions[bus].fn
+		inputs = make([]big.Int, fn.NumInputs())
+	)
+	// Intialise inputs values
+	for i := range fn.NumInputs() {
+		var (
+			ith big.Int
+			reg = fn.Register(schema.NewRegisterId(i))
+		)
+		// Initialise input from padding value
+		inputs[i] = *ith.Set(&reg.Padding)
+	}
+	// Compute function instance
+	return p.functions[bus].Call(inputs, p)
+}
+
+// Read implementation for the io.Map interface.
+func (p *Executor[T]) Read(bus uint, address []big.Int) []big.Int {
+	return p.functions[bus].Call(address, p).Outputs()
+}
+
+// Instances returns accrued function instances for the given bus.
+func (p *Executor[T]) Instances(bus uint) []FunctionInstance {
+	return p.functions[bus].instances
+}
+
+// Write implementation for the io.Map interface.
+func (p *Executor[T]) Write(bus uint, address []big.Int, values []big.Int) {
+	// At this stage, there no components use this functionality.
+	panic("unsupported operation")
+}
+
+// ============================================================================
+// FunctionTrace
+// ============================================================================
+
+// FunctionTrace captures all instances for a given function, and provides a
+// (thread-safe) API for calling to compute its output for a given set of
+// inputs.
+type FunctionTrace[T Instruction[T]] struct {
+	fn        *Function[T]
+	instances set.AnySortedSet[FunctionInstance]
+}
+
+// NewFunctionTrace constructs an empty trace for a given function.
+func NewFunctionTrace[T Instruction[T]](fn *Function[T]) *FunctionTrace[T] {
+	instances := set.NewAnySortedSet[FunctionInstance]()
+	//
+	return &FunctionTrace[T]{
+		fn,
+		*instances,
+	}
+}
+
+// Call this function to determine its outputs for a given set of inputs.  If
+// this instance has been seen before, it will simply return that.  Otherwise,
+// it will execute the function to determine the correct outputs.
+func (p *FunctionTrace[T]) Call(inputs []big.Int, iomap Map) FunctionInstance {
+	var iostate = FunctionInstance{uint(len(inputs)), inputs}
+	// Check whether this instance has already been computed.
+	if index := p.instances.Find(iostate); index != math.MaxUint {
+		// Yes, therefore return precomputed outputs
+		return p.instances[index]
+	}
+	// Execute function to determine new outputs.
+	return p.executeCall(inputs, iomap)
+}
+
+// Execute this function for a given set of inputs to determine its outputs and
+// produce a given instance.  The created instance is recorded within the trace
+// so it can be reused rather than recomputed in the future.  This function is
+// thread-safe, and will acquire the write lock on the cached instances
+// momentarily to insert the new instance.
+func (p *FunctionTrace[T]) executeCall(inputs []big.Int, iomap Map) FunctionInstance {
+	var (
+		fn = p.fn
+		// Determine how many I/O registers
+		nio = fn.NumInputs() + fn.NumOutputs()
+		//
+		pc = uint(0)
+		//
+		state = InitialState(inputs, fn.Registers(), fn.Buses(), iomap)
+	)
+	// Keep executing until we're done.
+	for pc != RETURN && pc != FAIL {
+		insn := fn.CodeAt(pc)
+		// execute given instruction
+		pc = insn.Execute(state)
+		// update state pc
+		state.Goto(pc)
+	}
+	// Cache I/O instance
+	instance := FunctionInstance{fn.NumInputs(), state.state[:nio]}
+	p.instances.Insert(instance)
+	// Done
+	return instance
+}
+
+// ============================================================================
+// FunctionInstance
+// ============================================================================
+
 // FunctionInstance captures the mapping from inputs (i.e. parameters) to outputs (i.e.
 // returns) for a particular instance of a given function.
 type FunctionInstance struct {
@@ -48,90 +172,4 @@ func (p FunctionInstance) Outputs() []big.Int {
 // Get value of given input or output argument for this instance.
 func (p FunctionInstance) Get(arg uint) big.Int {
 	return p.state[arg]
-}
-
-// Executor provides a mechanism for executing a program efficiently and
-// generating a suitable top-level trace.  Executor implements the io.Map
-// interface.
-type Executor[T Instruction[T]] struct {
-	program Program[T]
-	states  []set.AnySortedSet[FunctionInstance]
-}
-
-// NewExecutor constructs a new executor.
-func NewExecutor[T Instruction[T]](program Program[T]) *Executor[T] {
-	// Construct initially empty set of states
-	states := make([]set.AnySortedSet[FunctionInstance], len(program.Functions()))
-	// Construct new executor
-	return &Executor[T]{program, states}
-}
-
-// Instance returns a valid instance of the given bus.
-func (p *Executor[T]) Instance(bus uint) FunctionInstance {
-	var (
-		fn     = p.program.Function(bus)
-		inputs = make([]big.Int, fn.NumInputs())
-	)
-	// Intialise inputs values
-	for i := range fn.NumInputs() {
-		var (
-			ith big.Int
-			reg = fn.Register(schema.NewRegisterId(i))
-		)
-		// Initialise input from padding value
-		inputs[i] = *ith.Set(&reg.Padding)
-	}
-	// Compute function instance
-	return p.call(bus, inputs)
-}
-
-// Read implementation for the io.Map interface.
-func (p *Executor[T]) Read(bus uint, address []big.Int) []big.Int {
-	var (
-		iostate = FunctionInstance{uint(len(address)), address}
-		states  = p.states[bus]
-	)
-	// Check whether this instance has already been computed.
-	if index := states.Find(iostate); index != math.MaxUint {
-		// Yes, therefore return precomputed outputs
-		return states[index].Outputs()
-	}
-	// Execute function to determine new outputs.
-	return p.call(bus, address).Outputs()
-}
-
-// Instances returns accrued function instances for the given bus.
-func (p *Executor[T]) Instances(bus uint) []FunctionInstance {
-	return p.states[bus]
-}
-
-// Write implementation for the io.Map interface.
-func (p *Executor[T]) Write(bus uint, address []big.Int, values []big.Int) {
-	// At this stage, there no components use this functionality.
-	panic("unsupported operation")
-}
-
-func (p *Executor[T]) call(bus uint, inputs []big.Int) FunctionInstance {
-	var (
-		fn = p.program.Function(bus)
-		// Determine how many I/O registers
-		nio = fn.NumInputs() + fn.NumOutputs()
-		//
-		pc = uint(0)
-		//
-		state = InitialState(inputs, fn.Registers(), fn.Buses(), p)
-	)
-	// Keep executing until we're done.
-	for pc != RETURN && pc != FAIL {
-		insn := fn.CodeAt(pc)
-		// execute given instruction
-		pc = insn.Execute(state)
-		// update state pc
-		state.Goto(pc)
-	}
-	// Cache I/O instance
-	instance := FunctionInstance{fn.NumInputs(), state.state[:nio]}
-	p.states[bus].Insert(instance)
-	// Done
-	return instance
 }
