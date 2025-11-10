9f50ae339c713c70009e1384a36354c07042caac
DavePearce <dave01001110@gmail.com>
2025-11-10 15:21:01 +1300

    fix lookup test
    This fixes the new lookup test, which was previously missing a lot of
    tests.

--- testdata/corset/valid/lookup_28.lisp
+++ testdata/corset/valid/lookup_28.lisp
@@ -1,7 +1,7 @@
 (defcolumns (X :i16) (Y :i16) (P :i16))
-
-(defun (selector) (force-bin (- P (prev P))))
-
+(defpurefun (prev (X :any)) (shift X -1))
+;; define selector function
+(defun ((selector :binary :force)) (- P (prev P)))
 ;; example use of selector
 (defclookup l1 (Y) (selector) (X))
 
