ae475abc4b4746eab6c8f157b975209ad6ecc1ca
DavePearce <dave01001110@gmail.com>
2025-11-12 14:25:16 +1300

    fix bit_shr and bit_sar for KOALABEAR_16
    These utility functions were not able to compile under KOALABEAR_16 due
    to another known issues.  However, a workaround has been found for them
    which works for both the BLS12_377 and KOALABEAR_16 fields.

--- pkg/test/assembly_bench_test.go
+++ pkg/test/assembly_bench_test.go
@@ -27,6 +27,7 @@ func Test_AsmBench_Euc(t *testing.T) {
 	util.CheckWithFields(t, false, "asm/bench/euc", util.ASM_MAX_PADDING, field.BLS12_377)
 }
 func Test_AsmBench_Exp(t *testing.T) {
+	//#1226
 	util.CheckWithFields(t, false, "asm/bench/exp", util.ASM_MAX_PADDING, field.BLS12_377)
 }
 
@@ -35,6 +36,7 @@ func Test_AsmBench_Gas(t *testing.T) {
 }
 
 func Test_AsmBench_Shf(t *testing.T) {
+	//#1226
 	util.CheckWithFields(t, false, "asm/bench/shf", util.ASM_MAX_PADDING, field.BLS12_377)
 }
 
@@ -43,6 +45,7 @@ func Test_AsmBench_Stp(t *testing.T) {
 }
 
 func Test_AsmBench_Trm(t *testing.T) {
+	//#1319
 	util.CheckWithFields(t, false, "asm/bench/trm", util.ASM_MAX_PADDING, field.BLS12_377)
 }
 
@@ -50,7 +53,6 @@ func Test_AsmBench_Bin(t *testing.T) {
 	util.CheckWithFields(t, false, "asm/bench/bin", util.ASM_MAX_PADDING, field.BLS12_377, field.KOALABEAR_16)
 }
 
-// Field Element Out-Of-Bounds
 func Test_AsmBench_Wcp(t *testing.T) {
 	util.CheckWithFields(t, false, "asm/bench/wcp", util.ASM_MAX_PADDING, field.BLS12_377, field.KOALABEAR_16)
 }
