1cc9c5744a617a1734053800db39e0fdc54e006a
F Bojarski <ceciestunepoubelle@protonmail.ch>
2025-12-09 09:26:54 +0530

    ras fix test generation
    Signed-off-by: F Bojarski <ceciestunepoubelle@protonmail.ch>

--- testdata/asm/util/padding.go
+++ testdata/asm/util/padding.go
@@ -13,8 +13,8 @@ cat byte_counts_test.accepts | shuf | tail -n 5000 > testdata/asm/util/padding.a
 */
 
 func main() {
-	for i := uint(0); i < 17; i++ {
-		for j := uint(0); j < i+1; j++ {
+	for i := uint(0); i <= 16; i++ {
+		for j := uint(0); j <= i; j++ {
 			printInstance(i, j)
 		}
 	}
@@ -26,18 +26,24 @@ func printInstance(i uint, j uint) {
 		input   big.Int
 		res     big.Int
 	)
-	// input = 2^i + 2^j
+
+	// input = 256^(i-1) + 256^(j-1)
 	var a, b big.Int
-	a.SetBit(&a, int(i), 1)
-	b.SetBit(&b, int(j), 1)
+	if i != 0 {
+		a.SetBit(&a, int(8*(i-1)), 1)
+	}
+	if j != 0 {
+		b.SetBit(&b, int(8*(j-1)), 1)
+	}
 	input.Add(&a, &b)
 
 	// res = input >> (16 - i)
-	res.Rsh(&input, uint(16-i))
+	res.Lsh(&input, 8*uint(16-i))
 
 	builder.WriteString("{\"rpad_128\": { ")
 	builder.WriteString(fmt.Sprintf("\"input\": [%s]", input.String()))
 	builder.WriteString(fmt.Sprintf(", \"res\": [%s]", res.String()))
 	builder.WriteString(" }}")
 	fmt.Println(builder.String())
+
 }
