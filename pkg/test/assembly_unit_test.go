4a39a57029897467004d0197b6e6641f5824d4ac
David Pearce <david.pearce@consensys.net>
2025-12-17 11:06:44 +1300

    fix: vectorization of div instruction (#1412)
    fix vectorization of div instruction
    
    The division instruction was not being vectorized correctly, as its
    write conflicts were not accounted for.  This is now resolved, with a
    corresponding test added accordingly.

--- pkg/test/assembly_unit_test.go
+++ pkg/test/assembly_unit_test.go
@@ -68,6 +68,10 @@ func Test_AsmUnit_Div(t *testing.T) {
 	util.CheckWithFields(t, false, "asm/unit/div", util.ASM_MAX_PADDING, field.BLS12_377,
 		field.KOALABEAR_16, field.GF_8209)
 }
+func Test_AsmUnit_DivSeq(t *testing.T) {
+	util.CheckWithFields(t, false, "asm/unit/div_seq", util.ASM_MAX_PADDING, field.BLS12_377,
+		field.KOALABEAR_16, field.GF_8209)
+}
 func Test_AsmUnit_Div7(t *testing.T) {
 	util.CheckWithFields(t, false, "asm/unit/div7", util.ASM_MAX_PADDING, field.BLS12_377,
 		field.KOALABEAR_16, field.GF_8209)
