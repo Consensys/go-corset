d4efce1a84c63507822f127ad1833db64a8e25a3
DavePearce <dave01001110@gmail.com>
2026-01-05 09:35:20 +1300

    reset ite compilation pipeline

--- pkg/asm/io/macro/ite.go
+++ pkg/asm/io/macro/ite.go
@@ -13,13 +13,14 @@
 package macro
 
 import (
+	"errors"
 	"fmt"
 	"math/big"
 
 	"github.com/consensys/go-corset/pkg/asm/io"
+	"github.com/consensys/go-corset/pkg/asm/io/macro/expr"
 	"github.com/consensys/go-corset/pkg/asm/io/micro"
 	"github.com/consensys/go-corset/pkg/schema"
-	"github.com/consensys/go-corset/pkg/util/math"
 )
 
 // IfThenElse represents a ternary operation.
@@ -28,35 +29,35 @@ type IfThenElse struct {
 	// Cond indicates the condition
 	Cond uint8
 	// Left-hand side
-	Left io.RegisterId
+	Left expr.AtomicExpr
 	// Right-hand side
-	Right big.Int
+	Right expr.AtomicExpr
 	// Then/Else branches
-	Then, Else big.Int
+	Then, Else Expr
 }
 
 // Execute implementation for Instruction interface.
 func (p *IfThenElse) Execute(state io.State) uint {
 	var (
-		lhs   *big.Int = state.Load(p.Left)
-		rhs   *big.Int = &p.Right
+		lhs   = p.Left.Eval(state.Internal())
+		rhs   = p.Right.Eval(state.Internal())
 		value big.Int
 		taken bool
 	)
 	// Check whether taken or not.
 	switch p.Cond {
 	case EQ:
-		taken = lhs.Cmp(rhs) == 0
+		taken = lhs.Cmp(&rhs) == 0
 	case NEQ:
-		taken = lhs.Cmp(rhs) != 0
+		taken = lhs.Cmp(&rhs) != 0
 	default:
 		panic("unreachable")
 	}
 	//
 	if taken {
-		value = p.Then
+		value = p.Then.Eval(state.Internal())
 	} else {
-		value = p.Else
+		value = p.Else.Eval(state.Internal())
 	}
 	// Write value
 	state.StoreAcross(value, p.Targets...)
@@ -64,23 +65,71 @@ func (p *IfThenElse) Execute(state io.State) uint {
 	return state.Pc() + 1
 }
 
+// func (p *IfThenElse) Lower(pc uint) micro.Instruction {
+// 	code := &micro.Ite{
+// 		Targets: p.Targets,
+// 		Cond:    p.Cond,
+// 		Left:    p.Left,
+// 		Right:   p.Right,
+// 		Then:    p.Then,
+// 		Else:    p.Else,
+// 	}
+// 	//
+// 	return micro.NewInstruction(code, &micro.Jmp{Target: pc + 1})
+// }
+
 // Lower implementation for Instruction interface.
 func (p *IfThenElse) Lower(pc uint) micro.Instruction {
-	code := &micro.Ite{
-		Targets: p.Targets,
-		Cond:    p.Cond,
-		Left:    p.Left,
-		Right:   p.Right,
-		Then:    p.Then,
-		Else:    p.Else,
+	var (
+		codes      []micro.Code
+		thenBranch = p.Then.Polynomial()
+		elseBranch = p.Else.Polynomial()
+		lhs        io.RegisterId
+		rhs        micro.Expr
+	)
+	// normalise left / right
+	if c, ok := p.Left.(*expr.Const); ok {
+		lhs = p.Right.(*expr.RegAccess).Register
+		rhs = micro.NewConstant(c.Constant)
+	} else if c, ok := p.Right.(*expr.Const); ok {
+		lhs = p.Left.(*expr.RegAccess).Register
+		rhs = micro.NewConstant(c.Constant)
+	} else {
+		lhs = p.Left.(*expr.RegAccess).Register
+		rhs = p.Right.(*expr.RegAccess).ToMicroExpr()
 	}
 	//
-	return micro.NewInstruction(code, &micro.Jmp{Target: pc + 1})
+	switch p.Cond {
+	case EQ:
+		codes = []micro.Code{
+			&micro.Skip{Left: lhs, Right: rhs, Skip: 2},
+			// Then branch
+			&micro.Assign{Targets: p.Targets, Source: thenBranch},
+			&micro.Jmp{Target: pc + 1},
+			// Else branch
+			&micro.Assign{Targets: p.Targets, Source: elseBranch},
+			&micro.Jmp{Target: pc + 1},
+		}
+	case NEQ:
+		codes = []micro.Code{
+			&micro.Skip{Left: lhs, Right: rhs, Skip: 2},
+			// Then branch
+			&micro.Assign{Targets: p.Targets, Source: elseBranch},
+			&micro.Jmp{Target: pc + 1},
+			// Else branch
+			&micro.Assign{Targets: p.Targets, Source: thenBranch},
+			&micro.Jmp{Target: pc + 1},
+		}
+	default:
+		panic("unreachable")
+	}
+	//
+	return micro.Instruction{Codes: codes}
 }
 
 // RegistersRead implementation for Instruction interface.
 func (p *IfThenElse) RegistersRead() []io.RegisterId {
-	return []io.RegisterId{p.Left}
+	return expr.RegistersRead(p.Left, p.Right, p.Then, p.Else)
 }
 
 // RegistersWritten implementation for Instruction interface.
@@ -92,10 +141,10 @@ func (p *IfThenElse) String(fn schema.RegisterMap) string {
 	var (
 		regs    = fn.Registers()
 		targets = io.RegistersReversedToString(p.Targets, regs)
-		left    = regs[p.Left.Unwrap()].Name
-		right   = p.Right.String()
-		tb      = p.Then.String()
-		fb      = p.Else.String()
+		left    = p.Left.String(fn)
+		right   = p.Right.String(fn)
+		tb      = p.Then.String(fn)
+		fb      = p.Else.String(fn)
 		op      string
 	)
 	//
@@ -111,31 +160,23 @@ func (p *IfThenElse) String(fn schema.RegisterMap) string {
 	return fmt.Sprintf("%s = %s%s%s ? %s : %s", targets, left, op, right, tb, fb)
 }
 
-// Validate checks whether or not this instruction is correctly balanced.
+// Validate implementation for Instruction interface.
 func (p *IfThenElse) Validate(fieldWidth uint, fn schema.RegisterMap) error {
 	var (
-		regs     = fn.Registers()
-		lhs_bits = sumTargetBits(p.Targets, regs)
-		rhs_bits = sumThenElseBits(p.Then, p.Else)
+		regs                  = fn.Registers()
+		lhs_bits              = sumTargetBits(p.Targets, regs)
+		then_bits, thenSigned = expr.BitWidth(p.Then, fn)
+		else_bits, elseSigned = expr.BitWidth(p.Else, fn)
+		rhs_bits              = max(then_bits, else_bits)
 	)
 	// check
 	if lhs_bits < rhs_bits {
 		return fmt.Errorf("bit overflow (u%d into u%d)", rhs_bits, lhs_bits)
 	} else if rhs_bits > fieldWidth {
 		return fmt.Errorf("field overflow (u%d into u%d field)", rhs_bits, fieldWidth)
+	} else if thenSigned || elseSigned {
+		return errors.New("signed exprtession not supported here")
 	}
 	//
 	return io.CheckTargetRegisters(p.Targets, regs)
-}
-
-func sumThenElseBits(tb, fb big.Int) uint {
-	var (
-		tRange = math.NewInterval(tb, tb)
-		fRange = math.NewInterval(fb, fb)
-		values = tRange.Union(fRange)
-	)
-	// Observe, values cannot be negative by construction.
-	bitwidth, _ := values.BitWidth()
-	//
-	return bitwidth
 }
