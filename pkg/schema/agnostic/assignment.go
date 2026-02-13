db193d29f22dc81226eea80d9274840636a0765b
DavePearce <dave01001110@gmail.com>
2026-02-13 15:07:56 +1300

    revert assignment fix for now

--- pkg/schema/agnostic/assignment.go
+++ pkg/schema/agnostic/assignment.go
@@ -321,7 +321,7 @@ func determineRhsChunks(p StaticPolynomial, chunks []LhsChunk, field field.Confi
 		// Check whether chunk fits
 		if chunkWidth > field.BandWidth {
 			// No, it does not.
-			vars.Union(NonLinearReadSet(remainder))
+			vars.Union(RegisterReadSet(remainder))
 		} else if !last && chunkWidth > chunk.bitwidth {
 			// Overflow case.
 			p, chunk = propagateCarry(chunk, overflow, p, mapping)
