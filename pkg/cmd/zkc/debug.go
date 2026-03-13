fe646436e3bacaaccf26bffe0ba0b4eebfbdb462
DavePearce <dave01001110@gmail.com>
2026-03-13 16:29:33 +1300

    working on debug view
    Frankly, it needs work still.

--- pkg/cmd/zkc/debug.go
+++ pkg/cmd/zkc/debug.go
@@ -18,7 +18,6 @@ import (
 
 	"github.com/consensys/go-corset/pkg/schema/register"
 	"github.com/consensys/go-corset/pkg/trace"
-	"github.com/consensys/go-corset/pkg/util/collection/bit"
 	"github.com/consensys/go-corset/pkg/util/field"
 	"github.com/consensys/go-corset/pkg/util/field/bls12_377"
 	"github.com/consensys/go-corset/pkg/util/field/gf251"
@@ -50,27 +49,50 @@ var debugCmds = []FieldAgnosticCmd{
 }
 
 func runDebugCmd[F field.Element[F]](cmd *cobra.Command, args []string) {
-	width := GetUint(cmd, "width")
+	leftWidth := GetUint(cmd, "left-width")
+	midWidth := GetUint(cmd, "mid-width")
 	//
 	input := ParseInputFile(args[0])
 	// Compile source files, or print errors
 	program := CompileSourceFiles(args[1:]...)
 	//
-	executeIrProgram[*TraceObserver[word.Uint]]("main", program, input, &TraceObserver[word.Uint]{width: width})
+	observer := TraceObserver[word.Uint]{
+		leftPane: leftWidth,
+		midPane:  midWidth,
+	}
+	//
+	executeIrProgram[*TraceObserver[word.Uint]]("main", program, input, &observer)
+	//
+	fmt.Println()
 }
 
 // TraceObserver prints a trace
 type TraceObserver[W word.Word[W]] struct {
-	depth  uint
-	fun    *function.Function[instruction.Instruction[W]]
-	last   []string
-	width  uint
-	widths []uint
+	depth       uint
+	fun         *function.Function[instruction.Instruction[W]]
+	uses        string
+	definitions []register.Id
+	regWidth    uint
+	leftPane    uint
+	midPane     uint
 }
 
 // PreExecution implementation for Observer interface
 func (p *TraceObserver[W]) PreExecution(machine *machine.Base[W]) {
-
+	var (
+		n = machine.Depth()
+	)
+	//
+	if n > 0 {
+		if n != p.depth {
+			fmt.Println()
+			p.enterFunction(machine)
+			fmt.Print(p.callStack(machine))
+			fmt.Println()
+		}
+		//
+		p.writeInstruction(machine)
+	}
 }
 
 // PostExecution implementation for Observer interface
@@ -80,15 +102,10 @@ func (p *TraceObserver[W]) PostExecution(machine *machine.Base[W]) {
 	)
 	//
 	if n > 0 {
-		if n != p.depth {
-			fmt.Println()
-			p.enterFunction(machine)
-			p.writeFunctionTitle(machine)
-			fmt.Println()
+		if n == p.depth {
+			p.writeState(machine)
 		}
-		//
-		p.writeInstruction(machine)
-		p.writeState(machine, false)
+
 		fmt.Println()
 	}
 }
@@ -101,40 +118,13 @@ func (p *TraceObserver[W]) enterFunction(machine *machine.Base[W]) {
 	//
 	p.depth = n
 	p.fun = machine.Module(frame.Function()).(*function.Function[instruction.Instruction[W]])
-	p.last = nil
-	// construct widths
-	p.widths = make([]uint, p.fun.Width())
+	p.uses = ""
+	p.definitions = nil
+	p.regWidth = 0
 	//
-	for i, r := range p.fun.Registers() {
-		var (
-			nameLen = uint(len(r.Name()))
-			valLen  = 2 + (2 * bit.BytesRequiredFor(r.Width()))
-		)
-		//
-		if r.Width() == 0 {
-			valLen = 3
-		}
-		//
-		p.widths[i] = 1 + max(nameLen, valLen)
+	for _, r := range p.fun.Registers() {
+		p.regWidth = max(p.regWidth, uint(len(r.Name())))
 	}
-}
-
-func (p *TraceObserver[W]) writeFunctionTitle(machine *machine.Base[W]) {
-	var (
-		frame = machine.StackFrame(p.depth - 1)
-		pc    = frame.PC()
-	)
-	//
-	printLeftAligned(p.callStack(machine), p.width)
-	//
-	for i, r := range p.fun.Registers() {
-		printRightAligned(r.Name(), p.widths[i])
-	}
-	//
-	fmt.Println()
-	// Print initial state
-	printLeftAligned("", p.width)
-	p.writeState(machine, pc.First())
 }
 
 func (p *TraceObserver[W]) writeInstruction(machine *machine.Base[W]) {
@@ -144,82 +134,134 @@ func (p *TraceObserver[W]) writeInstruction(machine *machine.Base[W]) {
 		name    = trace.ParseModuleName("")
 		pc      = frame.PC()
 		insnStr = insn.String(register.ArrayMap(name, p.fun.Registers()...))
+		builder strings.Builder
 	)
 	//
 	insnStr = fmt.Sprintf("[%02x.%02x] %s", pc.Macro(), pc.Micro(), insnStr)
 	//
-	printLeftAligned(insnStr, p.width)
-}
-
-func (p *TraceObserver[W]) writeState(machine *machine.Base[W], inputs bool) {
-	var (
-		n     = machine.Depth()
-		frame = machine.StackFrame(n - 1)
-		last  = make([]string, p.fun.Width())
-	)
-	//
-	for i, r := range p.fun.Registers() {
+	fmt.Print(leftAligned(insnStr, p.leftPane))
+	// write uses
+	for i, r := range insn.Uses() {
+		var (
+			ith  = frame.Load(uint(i))
+			name = p.fun.Register(r).Name()
+		)
 		//
-		val := fmt.Sprintf("0x%s", frame.Load(uint(i)).Text(16))
-		// Record value for next instruction
-		last[i] = val
-		//
-		if p.last != nil && p.last[i] == val {
-			val = "."
+		if i != 0 {
+			builder.WriteString("; ")
 		}
 		//
-		if !inputs || r.IsInput() {
-			printRightAligned(val, p.widths[i])
-		}
+		builder.WriteString(fmt.Sprintf("%s==0x%s", name, ith.Text(16)))
 	}
 	//
-	p.last = last
+	p.uses = builder.String()
+	p.definitions = insn.Definitions()
+}
+
+func (p *TraceObserver[W]) writeState(machine *machine.Base[W]) {
+	fmt.Print(rightAligned(p.defs(machine), p.midPane))
+	//
+	if len(p.definitions) > 0 || p.uses != "" {
+		fmt.Printf(" ; %s", p.uses)
+	}
+}
+
+func (p *TraceObserver[W]) defs(machine *machine.Base[W]) string {
+	var (
+		n       = machine.Depth()
+		frame   = machine.StackFrame(n - 1)
+		builder strings.Builder
+	)
+	//
+	for i, r := range p.definitions {
+		var (
+			ith = frame.Load(r.Unwrap())
+			reg = p.fun.Register(r)
+		)
+		//
+		if i != 0 {
+			builder.WriteString("; ")
+		}
+		//
+		builder.WriteString(rightAligned(reg.Name(), p.regWidth))
+		builder.WriteString(":=0x")
+		builder.WriteString(ith.Text(16))
+	}
+	//
+	return builder.String()
 }
 
 func (p *TraceObserver[W]) callStack(machine *machine.Base[W]) string {
 	var builder strings.Builder
 	//
 	for i := uint(0); i < p.depth; i++ {
-		ith := machine.StackFrame(i)
-		fun := machine.Module(ith.Function())
-		builder.WriteString(fmt.Sprintf("> %s ", fun.Name()))
+		var (
+			ith = machine.StackFrame(i)
+			fun = machine.Module(ith.Function()).(*function.Function[instruction.Instruction[W]])
+		)
+		//
+		if i+1 == p.depth {
+			inputs := functionInputs(ith, fun)
+			builder.WriteString(fmt.Sprintf("> %s(%s) ", fun.Name(), inputs))
+		} else {
+			builder.WriteString(fmt.Sprintf("> %s ", fun.Name()))
+		}
 	}
 	//
 	return builder.String()
 }
 
-func printLeftAligned(str string, width uint) {
-	var (
-		n = min(uint(len(str)), width)
-	)
-	//
-	if uint(len(str)) > width {
-		str = str[:width-2]
-		fmt.Printf("%s..", str)
-	} else {
-		fmt.Print(str)
+func functionInputs[W word.Word[W]](frame machine.Frame[W], fun *function.Function[instruction.Instruction[W]]) string {
+	var builder strings.Builder
+
+	for i, r := range fun.Registers() {
+		var ith = frame.Load(uint(i))
+		//
+		if !r.IsInput() {
+			break
+		} else if i != 0 {
+			builder.WriteString(", ")
+		}
+		//
+		builder.WriteString(fmt.Sprintf("%s=0x%s", r.Name(), ith.Text(16)))
 	}
-	//
-	for i := n; i < width; i++ {
-		fmt.Printf(" ")
-	}
+
+	return builder.String()
 }
 
-func printRightAligned(str string, width uint) {
+func leftAligned(str string, width uint) string {
 	var (
-		n = min(uint(len(str)), width)
+		n       = min(uint(len(str)), width)
+		builder strings.Builder
 	)
-	//
-	for i := n; i < width; i++ {
-		fmt.Printf(" ")
-	}
 	//
 	if uint(len(str)) > width {
 		str = str[:width-2]
-		fmt.Printf("%s..", str)
+		builder.WriteString(fmt.Sprintf("%s..", str))
 	} else {
-		fmt.Print(str)
+		builder.WriteString(str)
 	}
+	//
+	for i := n; i < width; i++ {
+		builder.WriteString(" ")
+	}
+	//
+	return builder.String()
+}
+
+func rightAligned(str string, width uint) string {
+	var (
+		n       = min(uint(len(str)), width)
+		builder strings.Builder
+	)
+	//
+	for i := n; i < width; i++ {
+		builder.WriteString(" ")
+	}
+	//
+	builder.WriteString(str)
+	//
+	return builder.String()
 }
 
 func decode[W word.Word[W]](frame machine.Frame[W],
@@ -246,5 +288,6 @@ func decode[W word.Word[W]](frame machine.Frame[W],
 //nolint:errcheck
 func init() {
 	rootCmd.AddCommand(debugCmd)
-	debugCmd.Flags().Uint("width", 50, "width of instruction display panel")
+	debugCmd.Flags().Uint("left-width", 40, "width of instruction panel")
+	debugCmd.Flags().Uint("mid-width", 40, "width of assignment panel")
 }
