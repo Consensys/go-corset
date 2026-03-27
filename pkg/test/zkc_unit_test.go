27ca8e92af88401c7f81b89bde1b36be18d54a20
amkCha <29160563+amkCha@users.noreply.github.com>
2026-03-27 09:40:43 +0100

    1613 blake implementation in zkc  (#1617)
    * feat: implementation of Blake2 compression function F in ZkC, called blake,
            using static tables for initialization vector and sigma
            
    * feat: recursive implementation of switch_endian to swap between little- endians
            and big-endians
            
    * test: blake tests (r < 500) added in bench + switch_endian tests added in unit

--- pkg/test/zkc_unit_test.go
+++ pkg/test/zkc_unit_test.go
@@ -260,6 +260,10 @@ func Test_ZkcUnit_Static_01(t *testing.T) {
 	checkZkcUnit(t, "zkc/unit/static_01")
 }
 
+func Test_ZkcUnit_SwitchEndian(t *testing.T) {
+	checkZkcUnit(t, "zkc/unit/switch_endian")
+}
+
 // ===================================================================
 // Cast Tests
 // ===================================================================
