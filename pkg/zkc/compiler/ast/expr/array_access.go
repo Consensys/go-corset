d09a33489176d2e633b6567e14759d507ecf0c93
amkCha <29160563+amkCha@users.noreply.github.com>
2026-04-01 15:57:56 +0200

    feat: access read and write, parsing and linking, up to typing

---
+++ pkg/zkc/compiler/ast/expr/array_access.go
@@ -0,0 +1,60 @@
+// Copyright Consensys Software Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+// an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+package expr
+
+import (
+	"github.com/consensys/go-corset/pkg/util/collection/bit"
+	"github.com/consensys/go-corset/pkg/util/collection/set"
+	"github.com/consensys/go-corset/pkg/zkc/compiler/ast/data"
+	"github.com/consensys/go-corset/pkg/zkc/compiler/ast/symbol"
+	"github.com/consensys/go-corset/pkg/zkc/compiler/ast/variable"
+)
+
+// ArrayAccess represents an array access within an expression.
+type ArrayAccess[S symbol.Symbol[S]] struct {
+	Id       variable.Id
+	Args     []Expr[S]
+	datatype data.Type[S]
+}
+
+// NewArrayAccess constructs an expression representing an array access.
+func NewArrayAccess[S symbol.Symbol[S]](id variable.Id, args ...Expr[S]) Expr[S] {
+	return &ArrayAccess[S]{Id: id, Args: args}
+}
+
+// ExternUses implementation for the Expr interface.
+func (p *ArrayAccess[S]) ExternUses() set.AnySortedSet[S] {
+	return nil
+}
+
+// LocalUses implementation for the Expr interface.
+func (p *ArrayAccess[S]) LocalUses() bit.Set {
+	var read bit.Set
+	read.Insert(p.Id)
+	//
+	return read
+}
+
+func (p *ArrayAccess[S]) String(mapping variable.Map[S]) string {
+	return String[S](p, mapping)
+}
+
+// SetType implementation for Expr interface
+func (p *ArrayAccess[S]) SetType(t data.Type[S]) {
+	p.datatype = t
+}
+
+// Type implementation for Expr interface
+func (p *ArrayAccess[S]) Type() data.Type[S] {
+	return p.datatype
+}
