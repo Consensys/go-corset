be9dae0b8100bf17e3549491c425072025d3b47f
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2026-05-01 11:39:48 +0200

    fix: linting + wip switch_07 test
    The WIP test resembles a test suggested by @DavePearce but doesn't fully
    address it

--- pkg/test/zkc_unit_test.go
+++ pkg/test/zkc_unit_test.go
@@ -456,6 +456,10 @@ func Test_ZkcUnit_Switch_06(t *testing.T) {
 	checkZkcUnit(t, "zkc/unit/switch_06")
 }
 
+func Test_ZkcUnit_Switch_07(t *testing.T) {
+	checkZkcUnit(t, "zkc/unit/switch_07")
+}
+
 func Test_ZkcUnit_Switch_Numeric_01(t *testing.T) {
 	checkZkcUnit(t, "zkc/unit/switch_numeric_01")
 }
