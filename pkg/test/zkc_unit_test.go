d3fd8da9969807f2d4c48fe2fc5e464307a488ad
amkCha <29160563+amkCha@users.noreply.github.com>
2026-05-06 16:07:50 +0200

    add bit_ror64

--- pkg/test/zkc_unit_test.go
+++ pkg/test/zkc_unit_test.go
@@ -467,6 +467,10 @@ func Test_ZkcUnit_SwitchEndian(t *testing.T) {
 	checkZkcUnit(t, "zkc/unit/switch_endian", field.BLS12_377, field.KOALABEAR_16)
 }
 
+func Test_ZkcUnit_BitRor64(t *testing.T) {
+	checkZkcUnit(t, "zkc/unit/bit_ror64")
+}
+
 // ===================================================================
 // Cast Tests
 // ===================================================================
